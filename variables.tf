7ac47947b4e6e862ae2f12c288f9a3f2b07296ec
Olamide Olajide <34173234+olajio@users.noreply.github.com>
2020-11-23 08:57:41 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "profile" {
-  type = "string"
+  type = string
   default = "default"
 }
 
 variable "region-master" {
-  type = "string"
+  type = string
   default = "us-east-1"
 }
 
 variable "region-worker" {
-  type = "string"
+  type = string
   default = "us-west-2"
 }
